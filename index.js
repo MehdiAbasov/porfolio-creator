9a5b003f5bf6f730d97cc6c09dbf341cb87a1eed
AgayevElmin <agayevelmin73@gmail.com>
2023-02-11 22:01:18 +0400

    finished

--- index.js
+++ index.js
@@ -14,6 +14,28 @@ function openOverlay() {
 }
 
 function clsOverlay() {
-    overlay.style.width = '0%'  
+    overlay.style.width = '0%'
 }
 
+
+// img slider
+
+var slideImg = document.getElementById("slideImg");
+
+var images = new Array(
+    "images/support1.png",
+    "images/support2.png"
+);
+let length = images.length;
+let i = 0;
+
+function slider() {
+    if (i > length - 1) {
+        i = 0;
+    }
+    slideImg.src = images[i];
+    i++;
+    setTimeout('slider()', 2000)
+}
+
+window.onload = slider()
